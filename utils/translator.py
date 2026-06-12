41db9e0225809ac3846106320d4250ce4edbead8
Dhruva Gadegoni <dhruvagadegoni@gmail.com>
2026-06-12 14:31:12 +0530

    Fix quality tools configuration

--- utils/translator.py
+++ utils/translator.py
@@ -6,5 +6,5 @@ def load_language(lang):
     current_dir = os.path.dirname(os.path.dirname(__file__))
     path = os.path.join(current_dir, "locales", f"{lang}.json")
 
-    with open(path, "r", encoding="utf-8") as file:
+    with open(path, encoding="utf-8") as file:
         return json.load(file)
